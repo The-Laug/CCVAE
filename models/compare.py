3243f96cbc5f117e432515269a9f729d3893e471
Harborg1 <caharborg@gmail.com>
2025-12-07 00:25:30 +0100

    Some small changes

--- models/compare.py
+++ models/compare.py
@@ -31,6 +31,6 @@ def create_comparison(base_path, names: list[str]):
 
 if __name__ == "__main__":
     base_path = 'saves/CCVAE/permutation'
-    # create_comparison(base_path, ['hyperparam_test_lr_0.001_hd_512_ld_7', 'hyperparam_test_lr_0.001_hd_1024_ld_7', 'hyperparam_test_lr_0.0005_hd_1024_ld_7', 'hyperparam_test_lr_0.0005_hd_512_ld_7'])
-    create_comparison(base_path, ['hyperparam_test_lr_0.001_hd_512_ld_6', 'hyperparam_test_lr_0.001_hd_1024_ld_6', 'hyperparam_test_lr_0.0005_hd_1024_ld_6', 'hyperparam_test_lr_0.0005_hd_512_ld_6'])
+    create_comparison(base_path, ['hyperparam_test_lr_0.001_hd_512_ld_7', 'hyperparam_test_lr_0.001_hd_1024_ld_7', 'hyperparam_test_lr_0.0005_hd_1024_ld_7', 'hyperparam_test_lr_0.0005_hd_512_ld_7'])
+   # create_comparison(base_path, ['hyperparam_test_lr_0.001_hd_512_ld_', 'hyperparam_test_lr_0.001_hd_1024_ld_6', 'hyperparam_test_lr_0.0005_hd_1024_ld_6', 'hyperparam_test_lr_0.0005_hd_512_ld_6'])
     # create_comparison(base_path, ['hyperparam_test_lr_0.001_hd_512_ld_7', 'hyperparam_test_lr_0.001_hd_1024_ld_7'])
